9fa47644e4320c781fb84c04a4a7b46498946067
die-espin1 <die.espin.1980@gmail.com>
2026-04-01 10:03:20 -0600

    Cambiar el starcommand en render.yaml

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ def procesar():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
